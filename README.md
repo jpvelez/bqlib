9bb6a850aed347af5db1b1e809a3f291a43c9d6f
Ian Anderson <iananderson@spotify.com>
2016-01-07 17:41:37 -0500

    Initial Commit
    Initial commit of bqlib python files

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# bqlib
+# bqlib #
+
+This repository has the needed processes to interface with BigQuery
+via Python. Before any processes can be run, the installation process
+must be completed.
+
+## Installation ##
+
+First, you need to enable Google Cloud SDK and Python API access
+following the instructions (see <https://cloud.google.com/compute/docs/tutorials/python-guide>).
+Make sure that you add the BQ repository while running ```gcloud init```.
+
+Lastly, run these commands in the bqlib directory:
+
+```
+export dir=`python -m site --user-site`
+mkdir -p $dir
+cp bqlib.py $dir
+```
+
+## Included Subprocesses ##
+
+* add_bq_dataset(dsname): Creates a dataset at a given BQ location
+* add_bq_tbl_from_qry(qry,tbl): For a given query (qry), generates a BQ table with
+  name (tbl).
+* add_bq_vw_from_qry(qry,vw): For a given query (qry), generates a BQ view with
+  name (vw).
+* add_bq_tbl_from_csv(file,tbl,schema): Uploads a csv (file) to a BQ table with
+  name (tbl) using schema (schema).
+* remove_from_bq(obj): Removes an object (obj) from BQ repository.
+* query_bq(qry): Interactively runs BQ query (qry).
---
+++ bqlib.py
@@ -0,0 +1,59 @@
+import subprocess
+import csv
+
+def run_subprocess(cmd):
+	p = subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=True)
+	output = p.stdout.read()
+	print output
+
+def add_bq_dataset(dsname):
+	mycmd = "bq mk -d --data_location=EU %s" % (dsname)
+	print "Creating dataset " + dsname + " with " + mycmd
+	run_subprocess(mycmd)
+		
+def add_bq_tbl_from_qry(qry, tbl):
+	mycmd = "bq query --allow_large_results --replace --destination_table="+tbl + " \"" + qry + "\""
+	print "For tbl " + tbl + " calling " + mycmd
+	run_subprocess(mycmd)
+
+def add_bq_vw_from_qry(qry, vw):
+	mycmd = "bq mk --view=\"" + qry + "\" -f " + vw
+	print "For view " + vw + " calling " + mycmd
+	run_subprocess(mycmd)
+
+def add_bq_tbl_from_csv(file, tbl, schema):
+	mycmd = "bq load --encoding=UTF-8 --source_format=CSV \"" + tbl + "\" \"" + file + "\" \"" + schema + "\""	
+	print "For tbl " + tbl + " calling " + mycmd
+	run_subprocess(mycmd)
+
+def remove_from_bq(obj):
+	mycmd = "bq rm -f %s" % (obj)	
+	print "For obj " + obj + " calling " + mycmd
+	run_subprocess(mycmd)
+	
+def query_bq(qry):
+	encoding = 'utf-8'	  # specify the encoding of the CSV data
+	mycmd="bq query --max_rows=1000000000 --format=csv \"%s\"" % (qry)
+	print "Sending a query to subprocess:", mycmd
+	p2 = subprocess.Popen(mycmd, stdout=subprocess.PIPE, shell=True, stderr=subprocess.STDOUT)
+	output = p2.communicate()[0].decode(encoding)
+	edits = csv.reader(output.splitlines(), delimiter=",")
+	rownum=1
+	rownames=None
+	header_found=True
+	for row in edits:
+		if not header_found:
+			if rownum==1:
+				if row[0].endswith("Error processing job"):
+					print row[0]
+					for rw in edits:
+						print ' '.join(rw)
+					return
+				else:
+					rownames=row
+					rownum=rownum+1
+			else:
+				yield(dict(zip(rownames,row)))
+				rownum=rownum+1
+		if len(row)==1 and row[0].find("Current status: DONE") > 0:
+			header_found=False
