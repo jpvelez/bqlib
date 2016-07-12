fde813019da02db1c9ea7fe9f129ccad8d7c3505
Juan-Pablo Velez <jpvelez@spotify.com>
2016-07-12 17:43:17 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='bqlib',
+      version='0.1',
+      description='A python wrapper over BigQuery',
+      url='https://ghe.spotify.net/iananderson/bqlib',
+      author='Ian Anderson, Manish Nag',
+      author_email='manish@spotify.com',
+      license='MIT',
+      packages=['bqlib'])
