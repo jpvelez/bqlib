d0dee175dd4f8bcbdcea00e84eefb4093eaee507
Juan-Pablo Velez <jpvelez@spotify.com>
2016-07-27 16:48:20 -0400

    Remove packages from setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name = 'bqlib',
       version = '0.1',
       description = 'A simple python wrapper for Google BigQuery',
-      url = 'https://ghe.spotify.net/iananderson/bqlib',
+      url = 'https://github.com/jpvelez/bqlib',
+      download_url = 'https://github.com/jpvelez/bqlib/tarball/0.1',
       author = 'Manish Nag',
       author_email = 'manish@spotify.com',
       keywords = ['BigQuery', 'Google Cloud Platform'],
-      license = 'MIT',
-      packages = ['bqlib'])
+      license = 'MIT')
